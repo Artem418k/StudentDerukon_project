045240239c677311bfccee4397f54a39ff4509d5
Artem418k <125126012+Artem418k@users.noreply.github.com>
2023-05-25 04:28:34 +0300

    Add files via upload

---
+++ lab_2_Derukon.py
@@ -0,0 +1,21 @@
+#Завдання №1
+a1 = "Programming"
+a2 = "Essentials"
+a3 = "in...Python"
+result = a1 + "***" + a2 + "***" + a3
+print(result)
+
+#Завдання №2
+print("         *\n        * *\n       *   *\n      *     *\n     ***   ***\n       *   *\n       *   *\n       *****")
+
+#Завдання №3
+print("I'm student")
+
+#Завдання №4
+print("\"I'm\"\n\"\"learning\"\"\n\"\"\"Python\"\"\"")
+
+#Завдання №5
+print(0o500)
+
+#Завдання №6
+print(0x777)
